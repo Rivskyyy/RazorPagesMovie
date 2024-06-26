8984b3b0b30d61670b11418fe7fa62288c01c7de
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-06-26 22:02:18 +0300

    added model

---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
