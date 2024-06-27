57de0fb86c791469911e99d9b035dd0df62639e9
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-06-27 11:47:24 +0300

    added SearchString

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -6,9 +7,13 @@ namespace RazorPagesMovie.Models
     {
         public int id { get; set; }
         public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2")]
         public decimal Price { get; set; }
 
     }
